56421abe1dee05c4a09fe4429e99a7292a2a684d
olasocristian06-crypto <olaso.cristian06@gmail.com>
2026-05-01 22:58:33 +0800

    append multiple data entries to the log

--- personal diary.py
+++ personal diary.py
@@ -7,3 +7,25 @@ if not os.path.exists(DIARY_FILE):
     file = open(DIARY_FILE, "w")
     file.write("=== This is our Personal Diary System ===\n")
     file.close()
+
+while True:
+    print("=== Personal Diary System ===")
+    print("1. Add Multiple Entries")
+    print("2. View All Entries")
+    print("3. Search Entry")
+    print("4. Update Entry")
+    print("5. Delete Entry")
+    print("6. Line Count")
+    print("7. Exit")
+    
+    choice = input("Enter your choice (1-7): ")
+    
+    if choice == "1":
+        count = int(input("How many entries do you want to add? "))
+        file = open(DIARY_FILE, "a")
+        for i in range(count):
+            entry = input("Entry " + str(i+1) + ": ")
+            current_date = datetime.now().strftime("%Y-%m-%d %H:%M")
+            file.write(current_date + " | " + entry + "\n")
+        file.close()
+        print(str(count) + " entry/entries added!\n")
